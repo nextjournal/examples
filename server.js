8fde350e947eb12f4febb19e44c470ed547abae4
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-02-15 04:13:48 +0100

    ProseEditor example.

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ var server = require('substance/util/server');
 server.serveStyles(app, '/notepad/app.css', path.join(__dirname, 'notepad', 'app.scss'));
 server.serveJS(app, '/notepad/app.js', path.join(__dirname, 'notepad', 'app.js'));
 
+server.serveStyles(app, '/prose/app.css', path.join(__dirname, 'prose', 'app.scss'));
+server.serveJS(app, '/prose/app.js', path.join(__dirname, 'prose', 'app.js'));
+
 // Serve static files
 app.use(express.static(path.join(__dirname)));
 app.use('/fonts', express.static(path.join(__dirname, 'node_modules/font-awesome/fonts')));
